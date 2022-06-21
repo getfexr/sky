ff43e0df68119ab4b455131a892b7fa77dabf393
Nidhin Mahesh A <nidhin.m@rubix.network>
2022-06-21 17:09:55 +0530

    updated file structrue
    Signed-off-by: Nidhin Mahesh A <nidhin.m@rubix.network>

---
+++ fexr.go
@@ -0,0 +1,17 @@
+package gofexr
+
+import (
+    // "context"
+    // "crypto/tls"
+    // "log"
+    // "net"
+
+    // "google.golang.org/grpc"
+    // "google.golang.org/grpc/credentials"
+    // "google.golang.org/grpc/examples/helloworld/helloworld"
+    // "google.golang.org/grpc/reflection"
+)
+
+func main() {
+	
+}
