b44916378376493b47041579434edecaa3aafcc8
Nidhin Mahesh A <nidhin.m@rubix.network>
2022-06-29 01:51:17 +0530

    minifed file structure
    Signed-off-by: Nidhin Mahesh A <nidhin.m@rubix.network>

--- fexr.go
+++ fexr.go
@@ -1,17 +1,45 @@
-package gofexr
+package main
 
 import (
-    // "context"
-    // "crypto/tls"
-    // "log"
-    // "net"
-
-    // "google.golang.org/grpc"
-    // "google.golang.org/grpc/credentials"
-    // "google.golang.org/grpc/examples/helloworld/helloworld"
-    // "google.golang.org/grpc/reflection"
+	"log"
+	"net"
+	"fmt"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	// "gofexr/sync-v1/pop"
 )
 
+const (
+	ConfigFile string = "config.json"
+)
+
+const (
+	version string = "0.6.0"
+)
+
+func ShowVersion() {
+	fmt.Printf("\n****************************************\n\n")
+	fmt.Printf("gofexr Version : %s\n", version)
+	fmt.Printf("\n****************************************\n\n")
+}
+
 func main() {
-	
+
+	lis, err := net.Listen("tcp", ":6942")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	serverCert, err := credentials.NewServerTLSFromFile("../certs/server.crt","../certs/server.key")
+	if err != nil {
+		log.Fatalln("failed to create cert", err)
+	}
+
+	grpcServer := grpc.NewServer(grpc.Creds(serverCert))
+
+	// passport.
+
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %s", err)
+	}
 }
---
+++ sync-v1/pop/passport_service.go
@@ -0,0 +1,5 @@
+package pop
+
+import (
+
+)
