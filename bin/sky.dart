8f5ebfb6276477ddeeacbc326cb3cbbc9ade9d94
Nidhin Mahesh A <nidhin.m@rubix.network>
2022-10-13 15:31:09 +0530

    Dart files
    Signed-off-by: Nidhin Mahesh A <nidhin.m@rubix.network>

---
+++ bin/sky.dart
@@ -0,0 +1,5 @@
+import 'package:sky/sky.dart' as sky;
+
+void main(List<String> arguments) {
+  print('Hello world: ${sky.calculate()}!');
+}
---
+++ test/sky_test.dart
@@ -0,0 +1,8 @@
+import 'package:sky/sky.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
