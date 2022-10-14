088c0b303eecc449b5859cc1fe51ef783533a92c
Nidhin Mahesh A <nidhin.m@rubix.network>
2022-10-14 19:21:17 +0530

    fix sky thread error
    Signed-off-by: Nidhin Mahesh A <nidhin.m@rubix.network>

--- lib/background.dart
+++ lib/background.dart
@@ -3,31 +3,34 @@ import 'dart:math';
 import 'package:cron/cron.dart';
 import 'package:hive/hive.dart';
 
-class Bg {
-  var codes = Hive.box('codes');
+discardOTP() async {
+  var codes = await Hive.openBox('codes');
+  final cron = Cron();
+  cron.schedule(Schedule.parse('5-7 * * * *'), () async {
+    codes.deleteAt(-1);
+  });
+  codes.close();
+}
 
-  discardOTP() async {
-    final cron = Cron();
-    cron.schedule(Schedule.parse('5-7 * * * *'), () async {
-      codes.deleteAt(-1);
-    });
-  }
+Future<int> genOTP() async {
+  var codes = await Hive.openBox('codes');
+  // Generate random 4 digit number
+  var rng = Random();
+  var otp = rng.nextInt(9999);
+  codes.put(otp, DateTime.now().toString());
+  codes.close();
+  discardOTP();
+  return otp;
+}
 
-  Future<int> genOTP() async {
-    // Generate random 4 digit number
-    var rng = Random();
-    var otp = rng.nextInt(9999);
-    codes.put(otp, DateTime.now().toString());
-    discardOTP();
-    return otp;
-  }
-
-  Future<bool> checkOTP(int otp) async {
-    // Check if OTP is valid
-    if (codes.containsKey(otp)) {
-      return true;
-    } else {
-      return false;
-    }
+Future<bool> checkOTP(int otp) async {
+  var codes = await Hive.openBox('codes');
+  // Check if OTP is valid
+  if (codes.containsKey(otp)) {
+    codes.close();
+    return true;
+  } else {
+    codes.close();
+    return false;
   }
 }
