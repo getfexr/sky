67346216551fa6a7015ae89ba6f8b036f3662e59
Nidhin Mahesh A <nidhin.m@rubix.network>
2022-10-15 04:45:17 +0530

    challenge request 1/2
    Signed-off-by: Nidhin Mahesh A <nidhin.m@rubix.network>

--- lib/background.dart
+++ lib/background.dart
@@ -2,6 +2,25 @@ import 'dart:math';
 
 import 'package:cron/cron.dart';
 import 'package:hive/hive.dart';
+import 'package:logger/logger.dart';
+
+class Bg {
+  var lg = Logger(
+    // filter:   LogFilter(),
+    output:  ConsoleOutput(),
+    printer: PrettyPrinter(
+        methodCount: 2, // number of method calls to be displayed
+        errorMethodCount: 8, // number of method calls if stacktrace is provided
+        lineLength: 120, // width of the output
+        colors: true, // Colorful log messages
+        printEmojis: true, // Print an emoji for each log message
+        printTime: true // Should each log print contain a timestamp
+        ),
+  );
+  dump() {
+    lg.toString();
+  }
+}
 
 discardOTP() async {
   var codes = await Hive.openBox('codes');
@@ -12,9 +31,21 @@ discardOTP() async {
   codes.close();
 }
 
-Future<int> genOTP() async {
+listOTP() async {
   var codes = await Hive.openBox('codes');
-  // Generate random 4 digit number
+  // print codes and time stamps in reverse order with index human readable dates
+  for (var i = codes.length - 1; i >= 0; i--) {
+    print(
+        '${i+1}th code is ${codes.keyAt(i)} created on ${DateTime.fromMillisecondsSinceEpoch(codes.keyAt(i)).toLocal()}');
+  }
+  codes.close();
+}
+
+skyInfo() async {
+}
+
+genOTP() async {
+  var codes = await Hive.openBox('codes');
   var rng = Random();
   var otp = rng.nextInt(9999);
   codes.put(otp, DateTime.now().toString());
