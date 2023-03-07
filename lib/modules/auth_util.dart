68a490a2fc916823e9ce848e040d6353e435005a
Allen Iype P Cherian <95470290+Allen-Cherian@users.noreply.github.com>
2023-03-07 19:56:19 +0530

    Remove package fexr (#32)

--- lib/modules/auth_util.dart
+++ lib/modules/auth_util.dart
@@ -1,12 +1,4 @@
-// Dart GRPC Authorization Middleware
-import 'dart:typed_data';
-
 import 'package:grpc/grpc.dart';
-import 'package:jaguar_jwt/jaguar_jwt.dart';
-import 'package:pointycastle/pointycastle.dart';
-import 'package:pointycastle/signers/ecdsa_signer.dart';
-import 'package:sky/modules/jwt.dart';
-import 'package:fexr/fexr.dart' as fexr;
 
 String extractBearerToken(ServiceCall call) {
   var token = call.clientMetadata!['authorization'];
--- lib/native_interaction/rubix/rubix_util.dart
+++ lib/native_interaction/rubix/rubix_util.dart
@@ -5,7 +5,6 @@ import 'package:grpc/grpc.dart';
 import 'package:jaguar_jwt/jaguar_jwt.dart';
 import 'package:pointycastle/signers/ecdsa_signer.dart';
 import 'package:sky/protogen/native-interaction/rubix-native.pb.dart';
-import 'package:fexr/fexr.dart' as fexr;
 import 'package:sky/config.dart';
 
 final String _secret = Config().jwtAuthSecret;
@@ -26,6 +25,7 @@ class Token {
 
   Token(this.token, this.expiry);
 }
+
 class RubixUtil {
   Future<ChallengeString> createDIDChallenge({required String publicKey}) {
     final challengeToken = ChallengeToken.get(publicKey: publicKey);
@@ -35,25 +35,25 @@ class RubixUtil {
   }
 
   bool verifySignature(
-    ECPublicKey publicKey, Uint8List messageBytes, Uint8List signaturebytes) {
-  var verifier = ECDSASigner()
-    ..init(
-      false,
-      PublicKeyParameter(publicKey),
-    );
-  var sequence = ASN1Sequence.fromBytes(signaturebytes);
-  var r = (sequence.elements![0] as ASN1Integer).integer;
-  var s = (sequence.elements![1] as ASN1Integer).integer;
-  var signature = ECSignature(r!, s!);
+      ECPublicKey publicKey, Uint8List messageBytes, Uint8List signaturebytes) {
+    var verifier = ECDSASigner()
+      ..init(
+        false,
+        PublicKeyParameter(publicKey),
+      );
+    var sequence = ASN1Sequence.fromBytes(signaturebytes);
+    var r = (sequence.elements![0] as ASN1Integer).integer;
+    var s = (sequence.elements![1] as ASN1Integer).integer;
+    var signature = ECSignature(r!, s!);
 
-  var verified = verifier.verifySignature(messageBytes, signature);
-  return verified;
-}
+    var verified = verifier.verifySignature(messageBytes, signature);
+    return verified;
+  }
 
-void ecdsaVerify({required String payload, required List<int> signature}){
+  void ecdsaVerify({required String payload, required List<int> signature}) {
     JwtClaim jwtClaim = ChallengeToken.verify(payload);
     String publicKeyString = jwtClaim.subject!;
-    var publicKey = fexr.KeyPair().publicKeyFromPem(publicKeyString);
+    var publicKey = publicKeyFromPem(publicKeyString);
     var payloadCodeUnits = payload.codeUnits;
     var payloadBytes = Uint8List.fromList(payloadCodeUnits);
     var signatureBytes = Uint8List.fromList(signature);
@@ -61,13 +61,20 @@ void ecdsaVerify({required String payload, required List<int> signature}){
     if (!verified) {
       throw GrpcError.unauthenticated('Failed to verify signature');
     }
-}
+  }
+
+  ECPublicKey publicKeyFromPem(String publicKeyPem) {
+    return CryptoUtils.ecPublicKeyFromPem(publicKeyPem);
+  }
 }
 
 class AccesToken {
   static final int _accessTokenMaxAge = 30; // days
 
-  static Token get({required String did, required String peerId, required String publicKey}) {
+  static Token get(
+      {required String did,
+      required String peerId,
+      required String publicKey}) {
     final JwtClaim claimSet = JwtClaim(
         issuer: _issuer,
         subject: did,
