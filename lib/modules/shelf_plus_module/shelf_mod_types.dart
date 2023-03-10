55beee71821efb5acc94ca863306c719815abca9
Vyshnav S Deepak <vyshnav.s.deepak@gmail.com>
2023-03-10 15:07:37 +0530

    Add shelf plus module, add rubix rest api handler

---
+++ lib/modules/shelf_plus_module/shelf_mod_types.dart
@@ -0,0 +1,27 @@
+import 'package:shelf_plus/shelf_plus.dart';
+
+// Enum class for HTTP verbs
+enum HttpVerb { get, post, put, delete, patch, head, options, trace }
+
+// Enum to string conversion
+extension HttpVerbExtension on HttpVerb {
+  String get string {
+    return toString().split('.').last;
+  }
+}
+
+class CustomRoute {
+  HttpVerb verb;
+  String route;
+  Function handler;
+  Middleware? localMiddleware;
+
+  CustomRoute(this.verb, this.route, this.handler, {this.localMiddleware});
+}
+
+class SubRoute {
+  String route;
+  List<CustomRoute> endpoints;
+
+  SubRoute(this.route, this.endpoints);
+}
