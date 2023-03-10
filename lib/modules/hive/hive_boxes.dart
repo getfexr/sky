f4b716d53005bf1074f48c7f9e233383a65a4fcf
Allen Iype P Cherian <allen.l@rubix.network>
2023-03-10 15:56:51 +0530

    Update hive box mapping

---
+++ lib/modules/hive/hive_boxes.dart
@@ -0,0 +1,9 @@
+/*
+  Note: This file helps us to keep track of the hiveboxes used in this project and thereby avoiding an overlap.
+*/
+
+enum HiveBox { 
+  /* HiveBoxes shoudn't be renamed because string form of the
+    enum values which comes as "HiveBoxes.key1" might be already in use.*/
+  rubix, 
+}
