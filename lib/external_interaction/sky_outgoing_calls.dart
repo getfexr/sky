c6d975b180c3949e6622f45ddaae9b3f0ebddaf4
Allen Iype P Cherian <allen.l@rubix.network>
2023-04-26 16:15:02 +0530

    Add sky url from .env

--- lib/external_interaction/sky_outgoing_calls.dart
+++ lib/external_interaction/sky_outgoing_calls.dart
@@ -1,15 +1,17 @@
 import 'dart:convert';
+import 'package:sky/config.dart';
 import 'package:sky/native_interaction/rubix/rubix_util.dart';
 import 'package:http/http.dart' as http;
 
 
 Future<bool> fireAuthCallback({required String did,required String peerId,required String callBackUrl,required Token orgAccessToken,required String sessionId,required String chain })async {
+  final skyUrl = Config().skyUrl;
     var bodyJsonStr = jsonEncode({
       'did': '$did.$peerId',
       'session_id': sessionId,
       'token': orgAccessToken.token,
       'chain': chain,
-      'skyURl': ''
+      'sky_url': skyUrl
     });
   try{
      var response = await http.post(
